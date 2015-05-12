79f8efbda61ae10ae78116cbfd2700ac99804039
zhangyi <165607919@qq.com>
2015-05-12 16:19:52 +0800

    新增 指定url加载到内容中 的方法

--- res/main/js/alink.js
+++ res/main/js/alink.js
@@ -5,6 +5,7 @@ define(function(require, exports, module){
   var layer = require('layer').layer;
   var menu = require('menu');
   var cookie = require("jquery_cookie");
+  var setting = require("cookieSetting");
 
   var defaults = {
     scope: 'body'
@@ -21,7 +22,7 @@ define(function(require, exports, module){
         $(".main_content").hide();
         $(".main_content").load(href,function(response,status,xhr){      //ajax页面加载
           layer.close(index);
-          // setting.setting();
+          setting.setting();
           $(".main_content").show();
           if(status === "success" && cookie("phone") === "true"){    //如果加载成功 再关闭菜单（也是防止打开子菜单时误关闭了菜单）
             menu.selectItem(obj.attr("href"));
@@ -36,7 +37,26 @@ define(function(require, exports, module){
     });
   }
 
+  function load(href){
+    var reg = /javascript|[^\w]/
+    if(href && reg.test(href)){ //判断是否合法 
+        var index = layer.load(null,0);
+        $(".main_content").hide();
+        $(".main_content").html("");
+        $(".main_content").load(href,function(response,status,xhr){      //ajax页面加载
+          layer.close(index);
+          setting.setting();
+          $(".main_content").show();
+          if(status === "success" && cookie("phone") === "true"){    //如果加载成功 再关闭菜单（也是防止打开子菜单时误关闭了菜单）
+            menu.selectItem(obj.attr("href"));
+            menu.togglePhoneMenu(false);
+          }
+        });
+      }
+  }
+
   return {
-    init:init
+    init:init,
+    load: load
   }
 });
